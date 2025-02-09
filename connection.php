bc34a400b17caf3af05183fdf2bfb706525aaeab
Aman Naik <149400408+amannaik12@users.noreply.github.com>
2025-02-09 13:10:19 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$database = new mysqli("localhost", "root", "", "sql_database_edoc");
+$database = new mysqli("localhost", "root", "", "edoc");
 if ($database->connect_error) {
     die("Connection failed:  " . $database->connect_error);
 }
